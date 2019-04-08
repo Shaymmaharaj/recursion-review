832438bf15751ade483e5b082bf32db38a57f12a
shaymmaharaj <shyammaharaj@gmail.com>
2019-04-08 16:28:06 -0700

    second

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+    var output = "["
+
+  if(Array.isArray(obj)){
+      for(var i = 0; i < obj.length; i++){
+        if(typeof obj[i] === "object"){
+          return stringifyJSON(obj[i])
+        } 
+      if(typeof obj[i] === "string"){
+          output += obj[i]
+      }
+      if(typeof obj[i] === "number"){
+        output += obj[i].toString()
+      }
+    }
+  }
+output += "]"
+return output
+
+
+
+    
+
+  // for(var key in obj){
+  //   if (typeof obj[key] === "object"){
+  //       return stringifyJSON(obj[key])
+  //   }
+  //   if(typeof obj[key] === "string"){
+  //       return Object.keys(obj).toString();
+  //   } 
+  //   if(typeof obj[key] === "number"){
+  //         return obj[key].toString()
+  //   }
+  // }
 };
